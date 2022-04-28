ce29b8a6e39305ea04a0acdfcabc2eaaab7c0c00
Javier Romero moreno <jaromero@c1r5s2.42malaga.com>
2022-04-28 18:13:32 +0200

    Actualización erjercicios desde 42

--- Libft_00/_pruebas/ft_calloc.c
+++ Libft_00/_pruebas/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jaromero <jaromero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 12:37:34 by jaromero          #+#    #+#             */
-/*   Updated: 2022/04/26 10:29:57 by jaromero         ###   ########.fr       */
+/*   Updated: 2022/04/28 17:57:49 by jaromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*p;
 
+	if (nmemb == __SIZE_MAX__ && size > 0)
+		return (NULL);
+	if (size == __SIZE_MAX__ && nmemb > 0)
+		return (NULL);
 	if (nmemb == 0 || size == 0)
 		return (p);
 	p = malloc(nmemb * size);
