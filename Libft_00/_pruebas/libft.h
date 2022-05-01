e9ba55147f1acef04dca2a966e5980b9c54b3280
javi <c4n1t0@gmail.com>
2022-05-02 00:33:49 +0200

    Actualizacion de ejercicios 01/05/2022

--- Libft_00/_pruebas/libft.h
+++ Libft_00/_pruebas/libft.h
@@ -6,7 +6,7 @@
 /*   By: jaromero <jaromero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 22:47:15 by jaromero          #+#    #+#             */
-/*   Updated: 2022/04/28 22:31:59 by jaromero         ###   ########.fr       */
+/*   Updated: 2022/05/01 21:21:02 by jaromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,4 +45,5 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 #endif
