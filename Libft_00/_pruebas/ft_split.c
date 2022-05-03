7a7667f07626a95b932c996a53b7350b135289da
javi <c4n1t0@gmail.com>
2022-05-03 23:06:19 +0200

    Actualización de ejercicios 03/05/2022

--- Libft_00/_pruebas/ft_split.c
+++ Libft_00/_pruebas/ft_split.c
@@ -6,17 +6,16 @@
 /*   By: jaromero <jaromero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 19:02:33 by jaromero          #+#    #+#             */
-/*   Updated: 2022/05/03 00:15:12 by jaromero         ###   ########.fr       */
+/*   Updated: 2022/05/03 23:05:18 by jaromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_count_reserv(char *str, char c)
+int	ft_count_reserv(char *str, char c)
 {
-	int		i;
-	int		word;
-	char	**p;
+	int	i;
+	int	word;
 
 	i = 0;
 	word = 0;
@@ -26,10 +25,9 @@ char	**ft_count_reserv(char *str, char c)
 			word++;
 		i++;
 	}
-	word = word + 2;
+	word++;
 	printf("%d\n", word);
-	p = (char **)malloc(sizeof(char) * word);
-	return (p);
+	return (word);
 }
 
 char	**ft_reserv(char *str, char **p, char c)
@@ -56,9 +54,9 @@ char	**ft_reserv(char *str, char **p, char c)
 	}
 	p[x] = malloc((ft_strlen(str) + 1) * sizeof(char));
 	ft_strlcpy(p[x], str, ft_strlen(str) + 1);
-	/*x++;
+	x++;
 	p[x] = malloc(1 * sizeof(char));
-	p[x][0] = '\0';*/
+	p[x] = 0;
 	return (p);
 }
 
@@ -66,16 +64,23 @@ char	**ft_split(char const *s, char c)
 {
 	char	*str;
 	char	**p;
+	int		i;
 
+	//unsigned int	i;
 	//str = ft_strtrim(s, &c);
+	if (!*s)
+		return (NULL);
 	str = (char *)s;
-	p = ft_count_reserv(str, c);
+	printf("%s\n", str);
+	i = ft_count_reserv(str, c);
+	if (!s || !(p = malloc((i + 1) * sizeof(char *))))
+		return (NULL);
 	return (ft_reserv(str, p, c));
 }
 
-/*int	main(void)
+int	main(void)
 {
-	char const s[] = " hola que tal amigo ";
+	char const s[] = "hola que tal amigo";
 	char c;
 	char **str;
 	int i;
@@ -83,10 +88,11 @@ char	**ft_split(char const *s, char c)
 	i = 0;
 	c = ' ';
 	str = ft_split(s, c);
-	while (str[i] != NULL)
-	{
-		printf("%s\n", str[i]);
-		i++;
-	}
+	printf("%s\n", str[0]);
+	printf("%s\n", str[1]);
+	printf("%s\n", str[2]);
+	printf("%s\n", str[3]);
+	printf("%s\n", str[4]);
+
 	return (0);
-}*/
+}
