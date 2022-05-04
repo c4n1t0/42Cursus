0a45bf5764ffa0c317f6ea61d7e717636d530f4f
Javier Romero moreno <jaromero@c1r12s1.42malaga.com>
2022-05-04 16:41:19 +0200

    Actualizacion de ejercicios 04/05/2022

--- Libft_00/_pruebas/ft_split.c
+++ Libft_00/_pruebas/ft_split.c
@@ -6,81 +6,99 @@
 /*   By: jaromero <jaromero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 19:02:33 by jaromero          #+#    #+#             */
-/*   Updated: 2022/05/03 23:05:18 by jaromero         ###   ########.fr       */
+/*   Updated: 2022/05/04 16:28:04 by jaromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_count_reserv(char *str, char c)
+static int	ft_count_word(char *s, char c)
 {
 	int	i;
 	int	word;
 
 	i = 0;
 	word = 0;
-	while (str[i] != '\0')
+	while (s[i] != '\0')
 	{
-		if (str[i] == c)
-			word++;
-		i++;
+		if (s[i] == c)
+		{
+			i++;
+			if (s[i] != c)
+				word++;
+		}
+		else
+			i++;
 	}
 	word++;
-	printf("%d\n", word);
 	return (word);
 }
 
-char	**ft_reserv(char *str, char **p, char c)
+static void	ft_reserv_word(char *str, char c, char **p)
 {
-	unsigned int	i;
-	unsigned int	x;
-	char			*q;
+	int		i;
+	char	*word;
+	char	*s;
+	int		x;
 
 	i = 0;
 	x = 0;
-	while (str[i] != '\0')
+	s = str;
+	printf("c = %c\n", c);
+	while (*str != '\0')
 	{
-		if (str[i] == c)
+		printf("hola%d\n", i);
+		if (*s == c)
 		{
-			q = &str[i] + 1;
-			str[i] = '\0';
-			p[x] = malloc((ft_strlen(str) + 1) * sizeof(char));
-			ft_strlcpy(p[x], str, ft_strlen(str) + 1);
-			str = q;
+			printf("		hola1\n");
+			word = malloc((i + 1) * sizeof(char));
 			i = 0;
+			while (str != s && *str != '\0')
+			{
+				word[i] = *str;
+				i++;
+				str++;
+			}
+			word[i] = '\0';
+			p[x] = word;
+			while (*str == c)
+				str++;
+			printf("%s\n", str);
+			s = str;
 			x++;
+			i = 0;
 		}
-		i++;
+		else
+		{
+			i++;
+			s++;
+			//str++;
+		}
 	}
-	p[x] = malloc((ft_strlen(str) + 1) * sizeof(char));
-	ft_strlcpy(p[x], str, ft_strlen(str) + 1);
-	x++;
-	p[x] = malloc(1 * sizeof(char));
-	p[x] = 0;
-	return (p);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	*str;
 	char	**p;
-	int		i;
+	int		words;
 
-	//unsigned int	i;
-	//str = ft_strtrim(s, &c);
-	if (!*s)
+	if (!s)
 		return (NULL);
-	str = (char *)s;
-	printf("%s\n", str);
-	i = ft_count_reserv(str, c);
-	if (!s || !(p = malloc((i + 1) * sizeof(char *))))
+	str = ft_strtrim(s, &c);
+	printf("despues del trim -%s-\n", str);
+	words = ft_count_word((char *)s, c);
+	p = malloc(words * sizeof(char *));
+	printf("Contador de palabras %d\n", words);
+	if (!p)
 		return (NULL);
-	return (ft_reserv(str, p, c));
+	ft_reserv_word(str, c, p);
+	return (p);
 }
 
 int	main(void)
 {
-	char const s[] = "hola que tal amigo";
+	char const s[] = "      hola  que   tal amigo     ";
 	char c;
 	char **str;
 	int i;
@@ -93,6 +111,8 @@ int	main(void)
 	printf("%s\n", str[2]);
 	printf("%s\n", str[3]);
 	printf("%s\n", str[4]);
+	printf("%s\n", str[5]);
+	printf("%s\n", str[6]);
 
 	return (0);
 }
--- Libft_00/ft_split/ft_split.c
+++ Libft_00/ft_split/ft_split.c
@@ -6,17 +6,16 @@
 /*   By: jaromero <jaromero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 19:02:33 by jaromero          #+#    #+#             */
-/*   Updated: 2022/05/02 23:22:44 by jaromero         ###   ########.fr       */
+/*   Updated: 2022/05/04 15:36:17 by jaromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_count_reserv(char *str, char c)
+int	ft_count_reserv(char *str, char c)
 {
-	int		i;
-	int		word;
-	char	**p;
+	int	i;
+	int	word;
 
 	i = 0;
 	word = 0;
@@ -27,8 +26,8 @@ char	**ft_count_reserv(char *str, char c)
 		i++;
 	}
 	word++;
-	p = (char **)malloc(sizeof(char) * word);
-	return (p);
+	printf("%d\n", word);
+	return (word);
 }
 
 char	**ft_reserv(char *str, char **p, char c)
@@ -41,7 +40,7 @@ char	**ft_reserv(char *str, char **p, char c)
 	x = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] == c || str[i] == '\0')
+		if (str[i] == c)
 		{
 			q = &str[i] + 1;
 			str[i] = '\0';
@@ -55,6 +54,9 @@ char	**ft_reserv(char *str, char **p, char c)
 	}
 	p[x] = malloc((ft_strlen(str) + 1) * sizeof(char));
 	ft_strlcpy(p[x], str, ft_strlen(str) + 1);
+	x++;
+	p[x] = malloc(1 * sizeof(char));
+	p[x] = 0;
 	return (p);
 }
 
@@ -62,19 +64,79 @@ char	**ft_split(char const *s, char c)
 {
 	char	*str;
 	char	**p;
+	int		i;
 
-	str = ft_strtrim(s, &c);
-	p = ft_count_reserv(str, c);
+	if (!*s)
+		return (NULL);
+	str = (char *)s;
+	printf("%s\n", str);
+	i = ft_count_reserv(str, c);
+	if (!s || !(p = malloc((i + 1) * sizeof(char *))))
+		return (NULL);
 	return (ft_reserv(str, p, c));
 }
 
 int	main(void)
 {
-	char const s[] = " hola que tal amigo ";
-	char c;
-	char **str;
+	char const	s[];
+	char		c;
+	char		**str;
+	int			i;
 
-	c = ' ';
+	s[] = "hola que tal amigo";
+	i = 0;
+	c = 'n';
 	str = ft_split(s, c);
+	printf("%s\n", str[0]);
+	printf("%s\n", str[1]);
+	printf("%s\n", str[2]);
 	printf("%s\n", str[3]);
+	printf("%s\n", str[4]);
+	return (0);
+}
+
+char	**ft_words_reserv(char **p, char *str, char c, int words)
+{
+	int i;
+	int a;
+	int x;
+	char *q;
+
+	x = 0;
+	i = 0;
+	a = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == c)
+		{
+			q = malloc(i * sizeof(char));
+			while (*str != c)
+			{
+				q[a] = *str;
+				a++;
+				str++;
+			}
+			q[a] = '\0';
+			p[x] = q;
+			str++;
+			while (*str == c)
+				str++;
+			i = 0;
+			a = 0;
+			x++;
+		}
+		i++;
+	}
+	q = malloc(i * sizeof(char));
+	while (*str != '\0')
+	{
+		q[a] = *str;
+		a++;
+		str++;
+	}
+	q[a] = '\0';
+	p[x] = q;
+	x++;
+	p[x] = NULL;
+	return (p);
 }
