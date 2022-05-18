b050ee6b90e9f5e6e1e7bcc63efb81eac7aba8cf
javi <c4n1t0@gmail.com>
2022-05-18 09:50:07 +0200

    Actualización de pruebas GNL

--- Get_next_line/get_next_line.c
+++ Get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jaromero <jaromero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 11:45:16 by jaromero          #+#    #+#             */
-/*   Updated: 2022/05/15 21:19:54 by jaromero         ###   ########.fr       */
+/*   Updated: 2022/05/17 14:00:32 by jaromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,50 +17,50 @@
 
 char	*get_next_line(int fd)
 {
-	char	*buff;
-	char	buffer[700];
-	ssize_t	rd;
-	int		count;
+	static char	*buff;
+	static int	d;
+	static char	buffer[BUFFER_SIZE];
+	ssize_t		rd;
+	int			count;
 
-	count = 4;
+	d = 0;
+	count = 0;
 	buff = buffer;
-	rd = read(fd, buff, BUFFER_SIZE);
-	//	if (fd < 0 || BUFFER_SIZE <= 0)
-	//		return (NULL);
-	//	rd = read(fd, buff, BUFFER_SIZE);
-	while (*buff != '\0' && count > 0)
+	printf("buff esta apuntando a: %s\n", buff);
+	//if (*buff == '\0')
+	rd = read(fd, buffer, BUFFER_SIZE);
+	//write(1, "rd del read: &rd\n", 15);
+	d = 1;
+	if (!buff)
+		return (NULL);
+	while (*buff != '\n' && count < rd)
 	{
-		if (*buff == '\n')
-			count--;
 		write(1, buff, 1);
 		buff++;
+		count++;
 	}
+	/*if (*buff == '\n')
+	{
+		write(1, "\n", 1);
+		//count++;
+	}*/
 	return (buff);
 }
 
-int	main(int ac, char **av)
+int	main(void)
 {
 	int		fd1;
 	char	*ptrbuff;
 	int		count;
 
-	count = 4;
+	count = 0;
 	fd1 = 0;
 	fd1 = open("./numbers.dict", O_RDONLY);
-	if (fd1 == -1)
-		write(1, "Error open file\n", 16);
-	else
+	while (count < 10)
+	{
 		ptrbuff = get_next_line(fd1);
-	if (!ptrbuff)
-		write(1, "Empty file\n", 11);
-	else
-		/*while (*ptrbuff != '\0' && count > 0)
-		{
-			if (*ptrbuff == '\n')
-				count--;
-			write(1, ptrbuff, 1);
-			ptrbuff++;
-		}*/
-		close(fd1);
+		count++;
+	}
+	close(fd1);
 	return (0);
 }
