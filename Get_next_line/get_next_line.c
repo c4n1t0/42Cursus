aeeebc9aca2a67ae387091885bef14830e01db5b
javi <c4n1t0@gmail.com>
2022-05-20 13:44:32 +0200

    Actualización de ejercicios

--- Get_next_line/get_next_line.c
+++ Get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jaromero <jaromero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 11:45:16 by jaromero          #+#    #+#             */
-/*   Updated: 2022/05/19 13:15:37 by jaromero         ###   ########.fr       */
+/*   Updated: 2022/05/20 13:42:14 by jaromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+char	*ft_save(char *str, char *p, int i)
+{
+	int	j;
+
+	j = 0;
+	while (i > 0)
+	{
+		p[j] = str[j];
+		j++;
+		i--;
+	}
+	p[j] = '\0';
+	return (p);
+}
+
 char	*get_next_line(int fd)
 {
 	char		*buff;
@@ -22,15 +37,19 @@ char	*get_next_line(int fd)
 	char		*str;
 	char		*p;
 	int			i;
+	char		*ptr;
+	int			j;
 
 	i = 0;
 	if (!buff)
-		buff = buffer;
+		buff = str;
+	//if (*buff != '\0')
+	//	str = ft_strjoin(str, buff);
 	while (*buff != '\n')
 	{
 		if (*buff != '\0')
 		{
-			p[i] = *buff;
+			//write(1, buff, 1);
 			buff++;
 			i++;
 		}
@@ -38,16 +57,35 @@ char	*get_next_line(int fd)
 		{
 			buff = buffer;
 			ft_read_fd(fd, buff);
-			//str = ft_strjoin(str, buff);
+			str = ft_strjoin(str, buff);
 			//p = ft_strchr(str, '\n');
 		}
 	}
-	p[i] = *buff;
-	i++;
-	p[i] = '\0';
-	printf("p = %s-hasta aqui \n", p);
+	//printf("str = \n%s\n", str);
+	if (*buff == '\n')
+		i++;
+	p = ft_calloc((i + 1), sizeof(char));
+	//printf("str = \n%s\n", str);
+	//ptr = ft_save(str, p, i);
+	j = 0;
+	//printf("contador %d\n", i);
+	while (i > 0)
+	{
+		p[j] = str[j];
+		if (str[j] != '\0')
+			j++;
+		i--;
+	}
+	//str[j] = '\n';
+	if (ft_strlen(buff) > 1)
+	{
+		free(str);
+		str = ft_strjoin(str, buff);
+	}
+	else
+		free(str);
 	buff++;
-	return (str);
+	return (p);
 }
 
 int	main(void)
@@ -59,11 +97,12 @@ int	main(void)
 	count = 0;
 	fd1 = 0;
 	fd1 = open("./numbers.dict", O_RDONLY);
-	while (count < 5)
+	while (count < 10)
 	{
 		ptrbuff = get_next_line(fd1);
 		if (!ptrbuff)
 			return (0);
+		printf("%s", ptrbuff);
 		count++;
 	}
 	close(fd1);
