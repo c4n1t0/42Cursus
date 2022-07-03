28d6e2423123d4f2b679016f8113a17508034041
jaromero <jaromero@iMac-de-jaromero.local>
2022-07-04 00:08:38 +0200

    Actualizacion push swap

--- push_swap/push_swap_app.c
+++ push_swap/push_swap_app.c
@@ -6,7 +6,7 @@
 /*   By: jaromero <jaromero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/26 12:10:47 by jaromero          #+#    #+#             */
-/*   Updated: 2022/07/03 14:47:33 by jaromero         ###   ########.fr       */
+/*   Updated: 2022/07/04 00:05:09 by jaromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,15 @@ void	ft_swap_5(int *ptrA, int *ptrB, int count_a, int count_b)
 	int	i;
 
 	i = 0;
-	while (ft_swap_validator(ptrA, count_a + 1) != 0)
+	ft_push_b(ptrA, ptrB, count_a, count_b);
+	count_a--;
+	count_b++;
+	ft_push_b(ptrA, ptrB, count_a, count_b);
+	count_a--;
+	count_b++;
+	if (ft_swap_validator(ptrA, count_a) != 0)
+		ft_swap_3(ptrA, count_a);
+	/*while (ft_swap_validator(ptrA, count_a + 1) != 0)
 	{
 		if (ptrA[0] < ptrA[1])
 			ft_rotate_a_ra(ptrA, count_a);
@@ -58,32 +66,46 @@ void	ft_swap_5(int *ptrA, int *ptrB, int count_a, int count_b)
 			count_a--;
 			count_b++;
 		}
-	}
-	//ft_print_stack(ptrA, count_a);
-	//ft_print_stack(ptrB, count_b);
-	while (ft_swap_validator(ptrA, count_a + 1) != 0 || count_b != 0)
+	}*/
+	ft_print_stack(ptrA, count_a);
+	ft_print_stack(ptrB, count_b);
+	while (ft_swap_validator(ptrA, count_a) != 0 || count_b != 0)
 	{
-		if (ptrA[0] < ptrB[0] && ft_look_big(ptrA, count_a) == 0)
+		if (ptrA[0] < ptrB[0] && ft_look_big(ptrA, count_a) == 0 && count_b > 0)
 		{
-			ft_rotate_a_ra(ptrA, count_a);
 			ft_push_a(ptrA, ptrB, count_a, count_b);
 			count_a++;
 			count_b--;
+		}
+		else if (ptrA[0] < ptrB[0] && ft_look_big(ptrA, count_a) != 0)
+		{
+			ft_rotate_a_ra(ptrA, count_a);
+			if (ptrA[0] > ptrB[0] && count_b > 0)
+			{
+				ft_push_a(ptrA, ptrB, count_a, count_b);
+				count_a++;
+				count_b--;
+			}
 		}
 		else if (ptrB[0] > ptrA[0] && ptrB[0] < ptrA[1])
 		{
-			//ft_rotate_a_ra(ptrA, count_a);
+			ft_rotate_a_ra(ptrA, count_a);
 			ft_push_a(ptrA, ptrB, count_a, count_b);
 			count_a++;
 			count_b--;
 		}
-		else
+		else if (ptrB[0] < ptrA[0] && ft_look_small(ptrA, count_a) == 0
+				&& count_b > 0)
 		{
-			ft_rotate_a_ra(ptrA, count_a);
+			ft_push_a(ptrA, ptrB, count_a, count_b);
+			count_a++;
+			count_b--;
 		}
+		else if (ptrB[0] < ptrA[0] && count_b > 0)
+			ft_reverse_rotate_a_rra(ptrA, count_a);
 	}
-	//ft_print_stack(ptrA, count_a);
-	//ft_print_stack(ptrB, count_b);
+	ft_print_stack(ptrA, count_a);
+	ft_print_stack(ptrB, count_b);
 }
 
 int	ft_push_swap(int *ptrA, int *ptrB, int count_a)
