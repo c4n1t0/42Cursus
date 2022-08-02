55431a294010c6839bea9a3c7da8e5be86595699
Javier Romero moreno <jaromero@c1r12s2.42malaga.com>
2022-08-02 17:47:00 +0200

    Actualizacion minitalk pruebas

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: jaromero <jaromero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 11:37:41 by jaromero          #+#    #+#             */
-/*   Updated: 2022/08/02 10:30:58 by jaromero         ###   ########.fr       */
+/*   Updated: 2022/08/02 16:29:05 by jaromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,6 @@ int	main(int argc, char **argv)
 	if (argc != 3)
 		return (write(1, "Error\n", 6), -1);
 	pid = ft_atoi(str);
+	kill(pid, SIGUSR1);
 	ft_printf("pid = %d\n", pid);
 }
